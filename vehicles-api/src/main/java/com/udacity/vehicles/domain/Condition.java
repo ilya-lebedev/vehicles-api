7159a58c84db57c9f883ba278b779d4306611d7e
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-07 09:55:43 +0300

    fix: Fix not updating the value for attribute condition

--- vehicles-api/src/main/java/com/udacity/vehicles/domain/Condition.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/domain/Condition.java
@@ -1,5 +1,7 @@
 package com.udacity.vehicles.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 /**
  * Available values for condition of a given car.
  */
@@ -7,4 +9,15 @@ public enum Condition {
 
     USED,
     NEW;
+
+    @JsonCreator
+    public static Condition fromString(String key) {
+        for (Condition type : Condition.values()) {
+            if (type.name().equalsIgnoreCase(key)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
 }
